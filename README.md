f29abc461ab806e03ecf3971f217ee74ca98aef1
chackAJMCPE <90846141+chackAJMCPE@users.noreply.github.com>
2024-07-01 21:16:45 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+Put this in the same directory as Miracle_Thunder.exe .
+Launcher.exe is your new launcher.
---
+++ sbf.ps1
@@ -0,0 +1,12 @@
+$miracleThunderPath = "C:\Gsm_X_Team\Miracle Thunder v2.82\Miracle_Thunder.exe"
+Set-Date 20/06/2020
+Start-Process $miracleThunderPath
+$processName = "Miracle_Thunder"
+do {
+    Start-Sleep -Seconds 1 # Check every 5 seconds
+} while (Get-Process $processName -ErrorAction SilentlyContinue)
+Stop-Service w32time -Force
+Start-Service w32time
+w32tm /config /manualpeerlist:"time.windows.com" /syncfromflags:MANUAL /reliable:YES /update
+w32tm /resync
+exit 0
